bfff9827ab65359c187126b63b67faa73bb43268
Leonard Theret <leonard.theret@edhec.com>
2020-11-23 14:18:20 +0100

    f

--- setup.py
+++ setup.py
@@ -13,4 +13,5 @@ setup(name='toolboxleo',
       # include_package_data: to install data from MANIFEST.in
       include_package_data=True,
       scripts=['scripts/toolboxleo-run'],
-      zip_safe=False)
+      zip_safe=False,
+      install_requires=requirements)
